8746bd9f0b85bf1052635f5274dd79feb4c30d82
maravena1 <maravena@leasity.cl>
2025-10-15 17:52:40 -0300

    fix: fix transform error

--- sign.go
+++ sign.go
@@ -193,8 +193,8 @@ func (ctx *SigningContext) constructSignedInfo(el *etree.Element, enveloped bool
 		envelopedTransform := ctx.createNamespacedElement(transforms, TransformTag)
 		envelopedTransform.CreateAttr(AlgorithmAttr, EnvelopedSignatureAltorithmId.String())
 	}
-	canonicalizationAlgorithm := ctx.createNamespacedElement(transforms, TransformTag)
-	canonicalizationAlgorithm.CreateAttr(AlgorithmAttr, string(ctx.Canonicalizer.Algorithm()))
+	// canonicalizationAlgorithm := ctx.createNamespacedElement(transforms, TransformTag)
+	// canonicalizationAlgorithm.CreateAttr(AlgorithmAttr, string(ctx.Canonicalizer.Algorithm()))
 
 	// /SignedInfo/Reference/DigestMethod
 	digestMethod := ctx.createNamespacedElement(reference, DigestMethodTag)
